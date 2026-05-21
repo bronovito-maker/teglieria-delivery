a7a8fe1090d41394f0732a9d383a604086834536
bronovito-maker <bronovito@gmail.com>
2026-05-21 10:21:36 +0200

    feat: add Facebook Pixel tracking and domain verification to layout metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata, Viewport } from "next";
+import Script from "next/script";
 import { Epilogue, Manrope } from "next/font/google";
 import "./globals.css";
 import PwaRegister from "@/components/PwaRegister";
@@ -109,6 +110,7 @@ export const metadata: Metadata = {
   },
   other: {
     "mobile-web-app-capable": "yes",
+    "facebook-domain-verification": "2sezuwyy4wc31harqysybzs6gpvivv",
   },
 };
 
@@ -124,6 +126,29 @@ export default function RootLayout({
   return (
     <html lang="it">
       <body className={`${manrope.className} ${manrope.variable} ${epilogue.variable} ${epilogueLogo.variable} antialiased`} data-gramm="false" data-gramm_editor="false">
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '955909697258843');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=955909697258843&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
         <PwaRegister />
         {children}
         <ZirelWidgetHomeOnly />
