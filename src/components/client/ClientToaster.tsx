858d9bdba429d004abd643fe69ff1c417bac0dde
bronovito-maker <bronovito@gmail.com>
2026-08-18 14:43:17 +0200

    fix: keep cart toast out of menu controls

--- src/components/client/ClientToaster.tsx
+++ src/components/client/ClientToaster.tsx
@@ -3,5 +3,15 @@
 import { Toaster } from "sonner";
 
 export default function ClientToaster() {
-  return <Toaster position="top-center" richColors />;
+  return (
+    <Toaster
+      position="bottom-center"
+      richColors
+      closeButton
+      duration={2600}
+      toastOptions={{
+        className: "!mb-20 sm:!mb-6",
+      }}
+    />
+  );
 }
